bc0384b383e86d8df5f1db7113aee8f70d65d962
Donald Mxolisi <mxolisi@Donald.Mxolisi>
2025-04-19 22:02:31 +0200

    Updated

--- src/components/requests.js
+++ src/components/requests.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 
-const url ="http://localhost:8080";
+const url ="http://localhost:5000";
 
 export const loginUser = async (Email , token) => {
   const urlApi = `${url}/api/auth/Login`;
